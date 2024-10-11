4cbacc05a930e3f60e4ba5975b30b6d57f1b1308
Daigoro Miri <daigoro87@hotmail.it>
2024-10-11 16:29:40 +0200

    abbozzato Statistics.svelte e StatCard.svelte

--- src/components/Statistics.svelte
+++ src/components/Statistics.svelte
@@ -1,24 +1,23 @@
 <script>
   import { onMount } from "svelte";
   import { matchStatistics } from "../stores/Stores"; // Importa lo store e la funzione
+import StatCard from "./StatCard.svelte";
 
   $matchStatistics;
   onMount(() => {
- 
     const modal = new bootstrap.Modal(document.getElementById('statisticsModal'));
     modal.show(); 
   });
 </script>
 
-<style>
- 
-</style>
 
 
   <div class=" modal fade" id="statisticsModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class=" modal-dialog">
       <div class=" modal-content">
-        <p>contenuto</p>
+        <!-- Qui vanno le due StatCArd -->
+         <StatCard />
+         <StatCard />
         <button type="button" class=" btn btn-secondary" data-bs-dismiss="modal">Close</button>
     </div>
   </div>
